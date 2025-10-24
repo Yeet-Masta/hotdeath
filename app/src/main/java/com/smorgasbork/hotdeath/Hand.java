2ab5edf49ab6f4a04977982e7ef0a6eee58d364a
klaviartur <_github@fambit.de>
2025-10-24 22:34:25 +0200

    fix sorting on repeated reveal

--- app/src/main/java/com/smorgasbork/hotdeath/Hand.java
+++ app/src/main/java/com/smorgasbork/hotdeath/Hand.java
@@ -90,6 +90,7 @@ public class Hand {
 		for (int i = m_firstUnrevealed; i < m_numCards; i++)
 		{
 			m_firstUnrevealed = i + 1;
+			sort();
 			m_player.m_game.getGameTable().moveCardToTable(m_cards[i], m_player.getSeat(), i == m_numCards - 1?2:120);
 		}
 	}
